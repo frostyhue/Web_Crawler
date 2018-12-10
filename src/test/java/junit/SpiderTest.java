1d404a2af2f0374e496be916d37d88b5a045e283
Tinchev,Rostislav R.I <I358097@fhict.nl>
2018-12-10 14:33:36 +0100

    ADD Spider test to see if max search pages limit has been set, test fails

---
+++ src/test/java/junit/SpiderTest.java
@@ -0,0 +1,14 @@
+package junit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SpiderTest {
+
+    @Test
+    public void testSpiderMaxPagesToSearchIfExceedsLimit() {
+        Spider testSpider = new Spider(5);
+        assertEquals(10,testSpider.getNrOfMaxPages);
+    }
+}
