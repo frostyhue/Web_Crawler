9840d91af9a5af734773d475651c3285dceedefb
Tinchev,Rostislav R.I <I358097@fhict.nl>
2018-12-10 15:11:51 +0100

    ADD constructor functionality added to throw exception when max pages is more than 10, test passes

--- src/main/java/junit/Spider.java
+++ src/main/java/junit/Spider.java
@@ -4,10 +4,14 @@ public class Spider {
     private int MAX_PAGES_TO_SEARCH;
 
     public Spider(int maxPages) {
+        if (maxPages > 10){
+            throw new IllegalArgumentException("Max pages limit allowed exceeded, limit is 10 you tried: " + maxPages + "!");
+        }
         MAX_PAGES_TO_SEARCH = maxPages;
     }
 
     public int getNrOfMaxPages(){
         return this.MAX_PAGES_TO_SEARCH;
     }
+    
 }
