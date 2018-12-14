55e45ffcaaed2c444718aa786433aebc6fb0d7fb
Tinchev,Rostislav R.I <I358097@fhict.nl>
2018-12-14 22:02:20 +0100

    ADD test method to SpiderTest to test if method getNextURL returns the first method in the pagesToVisit set, variables added to Spider class, test fails

--- src/main/java/junit/Spider.java
+++ src/main/java/junit/Spider.java
@@ -1,7 +1,14 @@
 package junit;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
 public class Spider {
     private int MAX_PAGES_TO_SEARCH;
+    public Set<String> pagesVisited = new HashSet<String>();
+    public List<String> pagesToVisit = new LinkedList<String>();
 
     public Spider(int maxPages) {
         if (maxPages > 10){
@@ -15,6 +22,10 @@ public class Spider {
 
     public int getNrOfMaxPages(){
         return this.MAX_PAGES_TO_SEARCH;
+    }
+
+    public String getNextURL(){
+        return null;
     }
 
 }
--- src/test/java/junit/SpiderTest.java
+++ src/test/java/junit/SpiderTest.java
@@ -1,7 +1,6 @@
 package junit;
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class SpiderTest {
@@ -20,5 +19,16 @@ public class SpiderTest {
     @Test(expected = IllegalArgumentException.class)
     public void testSpiderIfNotAllowsForMaxPagesToBeLessThan0() {
         Spider testSpider = new Spider(-1);
+    }
+
+    @Test
+    public void testIfNextURLReturnsURL() {
+        Spider testObj = new Spider(10);
+        testObj.pagesToVisit.add("https://testURL0.test");
+        testObj.pagesToVisit.add("https://testURL1.test");
+        testObj.pagesToVisit.add("https://testURL2.test");
+        testObj.pagesToVisit.add("https://testURL3.test");
+
+        assertEquals(testObj.getNextURL(), "https://testURL0.test");
     }
 }
