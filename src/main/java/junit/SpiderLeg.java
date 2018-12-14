39ee01a1ce7aa8a5d2402937bc5faa2c3e45030b
TeodorGenov <teodor.genov23@gmail.com>
2018-12-14 21:24:22 +0100

    Test class added, Test testSpiderLegCrawlMethod() fails.

---
+++ src/main/java/junit/SpiderLeg.java
@@ -0,0 +1,24 @@
+package junit;
+
+import org.jsoup.Jsoup;
+import org.jsoup.Connection;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.jsoup.nodes.Document;
+
+import javax.sound.sampled.SourceDataLine;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class SpiderLeg {
+    public SpiderLeg()
+    {
+
+    }
+
+    public boolean crawl(String url)
+    {
+        return false;
+    }
+}
---
+++ src/test/java/junit/SpiderLegTest.java
@@ -0,0 +1,15 @@
+package junit;
+import org.junit.Test;
+import junit.SpiderLeg;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class SpiderLegTest {
+    @Test
+    public void testSpiderLegCrawlMethod()
+    {
+        SpiderLeg leg = new SpiderLeg();
+        assertEquals(true, leg.crawl(""));
+    }
+}
