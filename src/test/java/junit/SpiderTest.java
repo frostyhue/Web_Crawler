7c991f604a3bd090dddb65605950ceea743baf61
Tinchev,Rostislav R.I <I358097@fhict.nl>
2018-12-15 02:45:10 +0100

    ADD mock test for getMovie's getYear method in SpiderTest

--- src/test/java/junit/SpiderTest.java
+++ src/test/java/junit/SpiderTest.java
@@ -81,4 +81,21 @@ public class SpiderTest {
         verify(m).getFormat();
     }
 
+    @Test
+    public void testMockMovieYear() {
+        Movie m = mock(Movie.class);
+        Spider sut = new Spider(10);
+
+        when(m.getDirector()).thenReturn(directorMovie);
+        when(m.getYear()).thenReturn(yearMovie);
+        when(m.getFormat()).thenReturn(formatMovie);
+        when(m.getGenre()).thenReturn(genreMovie);
+        when(m.getStars()).thenReturn(starsMovie);
+        when(m.getWriters()).thenReturn(writersMovie);
+
+
+        sut.getMovie(m.getGenre(), m.getFormat(), m.getYear(), m.getDirector(), m.getWriters(), m.getStars());
+        verify(m).getYear();
+    }
+
 }
