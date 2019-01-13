ce61c72ab554f9fa659edf0d552f7abc1de199cf
Tinchev,Rostislav R.I <I358097@fhict.nl>
2019-01-13 21:33:10 +0100

    ADD testIfContructorSetsVariablesProperly() test method to MovieTest class, test failes

--- src/test/java/junit/MovieTest.java
+++ src/test/java/junit/MovieTest.java
@@ -1,6 +1,10 @@
 package junit;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class MovieTest {
@@ -10,5 +14,24 @@ public class MovieTest {
         Movie movieObj = new Movie("Fantasy", "Blu-ray", "2001", "Peter Jackson");
 
         assertEquals("Peter Jackson", movieObj.getDirector());
+    }
+
+    @Test
+    public void testMovieIfConstructorSetsVariablesProperly(){
+        List<String> writersTest = new ArrayList<>();
+        writersTest.add("Peter");
+        writersTest.add("George");
+        List<String> starsTest = new ArrayList<>();
+        starsTest.add("Kosta");
+        starsTest.add("Zeus");
+        Movie movieObj = new Movie("Fantasy", "Blu-ray", "2001", "Peter Jackson", "Movie", writersTest, starsTest);
+
+        assertEquals("Fantasy", movieObj.getGenre());
+        assertEquals("Blu-ray", movieObj.getFormat());
+        assertEquals("2001", movieObj.getYear());
+        assertEquals("Peter Jackson", movieObj.getDirector());
+        assertEquals("Movie", movieObj.getCategory());
+        assertEquals(writersTest, movieObj.getWriters());
+        assertEquals(starsTest, movieObj.getStars());
     }
 }
