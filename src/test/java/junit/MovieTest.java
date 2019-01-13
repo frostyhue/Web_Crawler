faa2c181975ba04e19406c4492aae996a4e87f3a
Tinchev,Rostislav R.I <I358097@fhict.nl>
2019-01-13 23:24:11 +0100

    ADD mock tests to MovieTest class

--- src/test/java/junit/MovieTest.java
+++ src/test/java/junit/MovieTest.java
@@ -1,5 +1,6 @@
 package junit;
 
+import org.json.simple.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,6 +8,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class MovieTest {
 
@@ -52,4 +55,68 @@ public class MovieTest {
         assertEquals(movieObj.getJSONFile().size(), 8);
     }
 
+    /**
+     * Mock test for Movie title attribute.
+     */
+    @Test
+    public void testMovieTitle(){
+        Movie mockMovie = mock(Movie.class);
+
+        when(mockMovie.getTitle()).thenReturn("Lord of The Rings");
+
+        assertEquals(mockMovie.getTitle(), "Lord of The Rings");
+
+        when(mockMovie.getTitle()).thenReturn("Bambi");
+
+        assertEquals(mockMovie.getTitle(), "Bambi");
+
+        when(mockMovie.getTitle()).thenReturn("Snow White");
+
+        assertEquals(mockMovie.getTitle(), "Snow White");
+    }
+
+    /**
+     * Mock test for Movie stars attribute.
+     */
+    @Test
+    public void testMovieStars(){
+        Movie mockMovie = mock(Movie.class);
+
+        List<String> testList = new ArrayList<>();
+        testList.add("Frodo");
+        testList.add("Zelda");
+
+        when(mockMovie.getStars()).thenReturn(testList);
+
+        assertEquals(mockMovie.getStars(), testList);
+
+        testList = new ArrayList<>();
+        testList.add("Ganon");
+        testList.add("Kirito");
+
+        when(mockMovie.getStars()).thenReturn(testList);
+
+        assertEquals(mockMovie.getStars(), testList);
+
+        testList = new ArrayList<>();
+        testList.add("Link");
+        testList.add("Koopa Troopa");
+
+        when(mockMovie.getStars()).thenReturn(testList);
+
+        assertEquals(mockMovie.getStars(), testList);
+    }/**
+     * Mock test for Movie getJSONFile size by default.
+     */
+    @Test
+    public void testMovieJSONSizeDefault(){
+        Movie mockMovie = mock(Movie.class);
+
+        when(mockMovie.getFormat()).thenReturn("");
+        when(mockMovie.getCategory()).thenReturn("");
+        when(mockMovie.getYear()).thenReturn("");
+
+        assertEquals(mockMovie.getJSONFile().size(), 3);
+
+    }
 }
