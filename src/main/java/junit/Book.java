f0c1dca150cc03249a18794e4197e3061e719c35
TeodorGenov <teodor.genov23@gmail.com>
2019-01-13 20:38:46 +0100

    Adding getTitle() test for Book object.

---
+++ src/main/java/junit/Book.java
@@ -0,0 +1,17 @@
+package junit;
+
+public class Book {
+    private String title;
+    private String category;
+    private String genre;
+    private String format;
+    private String year;
+    private String authors;
+    private String publisher;
+    private String isbn;
+    public Book()
+    {
+
+    }
+
+}
