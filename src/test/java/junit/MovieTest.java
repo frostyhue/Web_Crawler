6b9bc27a7001e59a73c3327914f155fe40336733
Tinchev,Rostislav R.I <I358097@fhict.nl>
2019-01-13 21:14:04 +0100

    ADD MovieTest class, constructor test to see if director is set, test fails

---
+++ src/test/java/junit/MovieTest.java
@@ -0,0 +1,14 @@
+package junit;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MovieTest {
+
+    @Test
+    public void testMovieIfConstructorSetsDirector(){
+        Movie movieObj = new Movie("Fantasy", "Blu-ray", "2001", "Peter Jackson");
+
+        assertEquals("Peter Jackson", movieObj.director);
+    }
+}
